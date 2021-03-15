83ee8053c71df10b835c29ae93fc50a03439ce16
Taiki Endo <te316e89@gmail.com>
2021-03-15 15:13:25 +0900

    wip

--- arci/examples/mock.rs
+++ arci/examples/mock.rs
@@ -162,6 +162,8 @@ async fn main() {
     let joint_trajectory_clients = indexmap::indexmap! {
         "c1".to_owned() => DummyJointTrajectoryClient::new("c1".to_owned(), vec!["j1".to_owned(), "j2".to_owned()]),
         "c2".to_owned() => DummyJointTrajectoryClient::new("c2".to_owned(), vec!["j3".to_owned(), "j4".to_owned()]),
+        "c3".to_owned() => DummyJointTrajectoryClient::new("c3".to_owned(), vec!["j5".to_owned(), "j6".to_owned()]),
+        "c4".to_owned() => DummyJointTrajectoryClient::new("c4".to_owned(), vec!["j7".to_owned(), "j8".to_owned()]),
     };
     let speakers = indexmap::indexmap! {
         "s1".to_owned() => DummySpeaker::new("s1".to_owned()),
@@ -169,12 +171,14 @@ async fn main() {
     let robot_client = RobotClient::new(speakers, joint_trajectory_clients);
 
     let s1 = robot_client.speakers["s1"].clone();
+    let i = std::time::Instant::now();
     handles.push(
         async move {
             tokio::task::spawn_blocking(move || {
                 // 1/2/3
                 s1.speak("msg");
                 // 6
+                eprintln!("expected 300+0~30ms, actual {:?}", i.elapsed());
             })
             .await
             .unwrap()
@@ -184,6 +188,9 @@ async fn main() {
 
     let c1 = robot_client.joint_trajectory_clients["c1"].clone();
     let c2 = robot_client.joint_trajectory_clients["c2"].clone();
+    let c3 = robot_client.joint_trajectory_clients["c3"].clone();
+    let c4 = robot_client.joint_trajectory_clients["c4"].clone();
+    let i = std::time::Instant::now();
     handles.push(
         async move {
             // 1/2/3
@@ -191,13 +198,16 @@ async fn main() {
                 .await
                 .unwrap();
             // 4
+            eprintln!("expected 100+0~30ms, actual {:?}", i.elapsed());
             c1.send_joint_positions(vec![3.0, -10.0], Duration::from_millis(300))
                 .await
                 .unwrap();
             // 7
+            eprintln!("expected 400+0~30ms, actual {:?}", i.elapsed());
         }
         .boxed(),
     );
+    let i = std::time::Instant::now();
     handles.push(
         async move {
             // 1/2/3
@@ -205,10 +215,46 @@ async fn main() {
                 .await
                 .unwrap();
             // 5
+            eprintln!("expected 200+0~30ms, actual {:?}", i.elapsed());
             c2.send_joint_positions(vec![4.0, -10.0], Duration::from_millis(400))
                 .await
                 .unwrap();
             // 8
+            eprintln!("expected 600+0~30ms, actual {:?}", i.elapsed());
+        }
+        .boxed(),
+    );
+    let i = std::time::Instant::now();
+    handles.push(
+        async move {
+            // 1/2/3
+            c3.send_joint_positions(vec![2.0, -10.0], Duration::from_millis(150))
+                .await
+                .unwrap();
+            // 5
+            eprintln!("expected 150+0~30ms, actual {:?}", i.elapsed());
+            c3.send_joint_positions(vec![4.0, -10.0], Duration::from_millis(300))
+                .await
+                .unwrap();
+            // 8
+            eprintln!("expected 450+0~30ms, actual {:?}", i.elapsed());
+        }
+        .boxed(),
+    );
+    let i = std::time::Instant::now();
+    handles.push(
+        async move {
+            // 1/2/3
+            c4.send_joint_positions(vec![2.0, -10.0], Duration::from_millis(300))
+                .await
+                .unwrap();
+            // 5
+            eprintln!("expected 300+0~30ms, actual {:?}", i.elapsed());
+            c4.send_joint_positions(vec![4.0, -10.0], Duration::from_millis(200))
+                .await
+                .unwrap();
+            // 8
+            eprintln!("expected 500+0~30ms, actual {:?}", i.elapsed());
         }
         .boxed(),
     );
@@ -220,7 +266,7 @@ async fn main() {
     dbg!(result);
 
     // There is no guarantee which will be started first.
-    assert!(result[0..3].iter().all(|r| vec![
+    assert!(result[0..5].iter().all(|r| vec![
         SpeakStart {
             name: "s1".into(),
             message: "msg".into(),
@@ -235,27 +281,53 @@ async fn main() {
             positions: vec![2.0, -10.0],
             duration: Duration::from_millis(200)
         },
+        SendPositionsStart {
+            name: "c3".into(),
+            positions: vec![2.0, -10.0],
+            duration: Duration::from_millis(150)
+        },
+        SendPositionsStart {
+            name: "c4".into(),
+            positions: vec![2.0, -10.0],
+            duration: Duration::from_millis(300)
+        },
     ]
     .contains(r)));
 
-    assert_eq!(
-        result[3..],
-        vec![
-            SendPositionsEnd { name: "c1".into() },
-            SendPositionsStart {
-                name: "c1".into(),
-                positions: vec![3.0, -10.0],
-                duration: Duration::from_millis(300)
-            },
-            SendPositionsEnd { name: "c2".into() },
-            SendPositionsStart {
-                name: "c2".into(),
-                positions: vec![4.0, -10.0],
-                duration: Duration::from_millis(400)
-            },
-            SpeakEnd { name: "s1".into() },
-            SendPositionsEnd { name: "c1".into() },
-            SendPositionsEnd { name: "c2".into() },
-        ]
-    );
+    let expected = vec![
+        SendPositionsEnd { name: "c1".into() },
+        SendPositionsStart {
+            name: "c1".into(),
+            positions: vec![3.0, -10.0],
+            duration: Duration::from_millis(300),
+        },
+        SendPositionsEnd { name: "c3".into() },
+        SendPositionsStart {
+            name: "c3".into(),
+            positions: vec![4.0, -10.0],
+            duration: Duration::from_millis(300),
+        },
+        SendPositionsEnd { name: "c2".into() },
+        SendPositionsStart {
+            name: "c2".into(),
+            positions: vec![4.0, -10.0],
+            duration: Duration::from_millis(400),
+        },
+        SpeakEnd { name: "s1".into() },
+        SendPositionsEnd { name: "c4".into() },
+        SendPositionsStart {
+            name: "c4".into(),
+            positions: vec![4.0, -10.0],
+            duration: Duration::from_millis(200),
+        },
+        SendPositionsEnd { name: "c1".into() },
+        SendPositionsEnd { name: "c3".into() },
+        SendPositionsEnd { name: "c4".into() },
+        SendPositionsEnd { name: "c2".into() },
+    ];
+    assert_eq!(result[5..].len(), expected.len());
+
+    for (i, (expected, result)) in expected.iter().zip(result[5..].iter()).enumerate() {
+        assert_eq!(expected, result, "{}", i);
+    }
 }
